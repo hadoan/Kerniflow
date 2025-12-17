3482539613b04c483dadb4faca1b93a27deec8b3
Ha Doan <ha.doanmanh@gmail.com>
2025-12-17 12:57:08 +0100

    fix docker

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,3 +1,4 @@
+import "./load-env";
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
@@ -18,14 +19,14 @@ async function bootstrap() {
   SwaggerModule.setup("docs", app, document);
 
   await app.listen(3000);
-  // eslint-disable-next-line no-console
+   
   console.log("[api] listening on http://localhost:3000");
-  // eslint-disable-next-line no-console
+   
   console.log("[api] swagger docs at http://localhost:3000/docs");
 }
 
 bootstrap().catch((err) => {
-  // eslint-disable-next-line no-console
+   
   console.error(err);
   process.exit(1);
 });
--- services/worker/src/modules/outbox/outbox.module.ts
+++ services/worker/src/modules/outbox/outbox.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
-import { OutboxPollerService } from './OutboxPollerService';
-import { OutboxRepository } from '@kerniflow/data';
+import { Module } from "@nestjs/common";
+import { OutboxPollerService } from "./OutboxPollerService";
+import { OutboxRepository } from "@kerniflow/data";
 
 @Module({
-  providers: [OutboxPollerService, OutboxRepository],
+  providers: [
+    OutboxPollerService,
+    {
+      provide: OutboxRepository,
+      useValue: new OutboxRepository(), // Explicit instance to avoid DI token resolution issues
+    },
+  ],
 })
 export class OutboxModule {}
